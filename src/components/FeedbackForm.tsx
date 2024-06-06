049ea24c2f30d2c4834303ae6cfb29eb305eec96
Schlumen <lumen.haendler@web.de>
2024-06-06 15:39:35 +0200

    Fetch data from API

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const charCount = MAX_CHARACTERS - text.length;
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value;
+    if (newText.length > MAX_CHARACTERS) return;
+    setText(newText);
+  };
+
   return (
     <form className="form">
-      <textarea id="feedback-textarea" placeholder="x" spellCheck={false} />
+      <textarea
+        value={text}
+        onChange={handleChange}
+        id="feedback-textarea"
+        placeholder="x"
+        spellCheck={false}
+        maxLength={MAX_CHARACTERS}
+      />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,25 +1,45 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
+import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchFeedbackItems = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+      );
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const data = await response.json();
+      setFeedbackItems(data.feedbacks);
+    } catch (error) {
+      setErrorMessage("Something went wrong. Please try again later.");
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFeedbackItems();
+  }, []);
+
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <TriangleUpIcon />
-          <span>456</span>
-        </button>
-        <div>
-          <p>B</p>
-        </div>
-        <div>
-          <p>Company</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-            maiores sint dicta soluta earum eum.
-          </p>
-        </div>
-        <p>4d</p>
-      </li>
+      {isLoading && <Spinner />}
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+      {feedbackItems.map(feedbackItem => (
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
+      ))}
     </ol>
   );
 }
