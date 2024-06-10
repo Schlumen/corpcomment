d21858903ded59a67d576e51203c501763426380
Schlumen <lumen.haendler@web.de>
2024-06-10 14:47:33 +0200

    Add Item, better layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,79 @@
-import Container from "./Container";
-import Footer from "./Footer";
+import { useEffect, useState } from "react";
+import { TFeedbackItem } from "../lib/types";
+import Container from "./layout/Container";
+import Footer from "./layout/Footer";
 import HashtagList from "./HashtagList";
 
 function App() {
+  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleAddToList = async (text: string) => {
+    const companyName = text
+      .split(" ")
+      .find(word => word.includes("#"))!
+      .substring(1);
+
+    const newItem: TFeedbackItem = {
+      id: new Date().getTime(),
+      text,
+      upvoteCount: 0,
+      daysAgo: 0,
+      company: companyName,
+      badgeLetter: companyName.substring(0, 1).toUpperCase(),
+    };
+
+    setFeedbackItems([...feedbackItems, newItem]);
+
+    await fetch(
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newItem),
+      }
+    );
+  };
+
+  const fetchFeedbackItems = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+      );
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const data = await response.json();
+      setFeedbackItems(data.feedbacks);
+    } catch (error) {
+      setErrorMessage("Something went wrong. Please try again later.");
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFeedbackItems();
+  }, []);
+
   return (
     <div className="app">
       <Footer />
 
-      <Container />
+      <Container
+        feedbackItems={feedbackItems}
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        handleAddToList={handleAddToList}
+      />
 
       <HashtagList />
     </div>
--- src/components/feedback/FeedbackForm.tsx
+++ src/components/feedback/FeedbackForm.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
-import { MAX_CHARACTERS } from "../lib/constants";
+import { MAX_CHARACTERS } from "../../lib/constants";
 
-export default function FeedbackForm() {
+type FeedbackFormProps = {
+  onAddToList: (text: string) => void;
+};
+
+export default function FeedbackForm({ onAddToList }: FeedbackFormProps) {
   const [text, setText] = useState("");
   const charCount = MAX_CHARACTERS - text.length;
 
@@ -11,8 +15,15 @@ export default function FeedbackForm() {
     setText(newText);
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (text.length === 0) return;
+    onAddToList(text);
+    setText("");
+  };
+
   return (
-    <form className="form">
+    <form onSubmit={handleSubmit} className="form">
       <textarea
         value={text}
         onChange={handleChange}
